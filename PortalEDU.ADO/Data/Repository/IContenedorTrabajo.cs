302045756862bb88844ca8c03133d1b81e314a8c
josueascencio <45615031+josueascencio@users.noreply.github.com>
2021-02-21 03:34:55 -0600

    Hasta Vista Categoria

--- PortalEDU.ADO/Data/Repository/IContenedorTrabajo.cs
+++ PortalEDU.ADO/Data/Repository/IContenedorTrabajo.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace PortalEDU.ADO.Data.Repository
 {
-    interface IContenedorTrabajo : IDisposable
+    public interface IContenedorTrabajo : IDisposable
     {
         ICategoriaRepository Categoria { get; }
 
---
+++ PortalEdu3_1/Areas/Admin/Controllers/CategoriasController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortalEDU.ADO.Data;
+using PortalEDU.ADO.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalEdu3_1.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class CategoriasController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+
+        public CategoriasController(IContenedorTrabajo contenedorTrabajo)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region LLAMADAS A LAS API
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            return Json(new {data = _contenedorTrabajo.Categoria.GetAll()});
+        }
+
+        #endregion
+
+
+
+    }
+}
