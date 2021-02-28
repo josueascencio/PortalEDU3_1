4ce772d914d0e7941cd4cb6d6fa7443352f01382
josueascencio <evolutioncompu@gmail.com>
2021-02-28 01:23:22 -0600

    HastaVideo58_PuntosDeDepuracion

---
+++ PortalEDU.ADO/Migrations/20210227035437_CreandoMigracionenOficina.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PortalEDU.ADO.Migrations
+{
+    public partial class CreandoMigracionenOficina : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
